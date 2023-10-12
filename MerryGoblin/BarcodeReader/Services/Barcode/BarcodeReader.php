623d31449743ec8f862e8ccca1b117d8121cd849
Keller Alexandre <keller.alexandre0@gmail.com>
2023-10-12 15:52:00 +0200

    Barcode reader ready for packagist

---
+++ MerryGoblin/BarcodeReader/Services/Barcode/BarcodeReader.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @link https://github.com/merry-goblin/barcode-reader-composer
+ */
+
+namespace MerryGoblin\BarcodeReader;
+
+use Symfony\Component\Process\Process;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class BarcodeReader
+{
+	public function __construct()
+	{
+
+	}
+
+	/**
+	 * @param string $fullFilePath
+	 * @return string
+	 * @throws ProcessFailedException
+	 */
+	public function parse($fullFilePath)
+	{
+		$process = new Process(['zbarimg', $fullFilePath]);
+		$process->run();
+
+		// executes after the command finishes
+		if (!$process->isSuccessful()) {
+			throw new ProcessFailedException($process);
+		}
+
+		return $process->getOutput();
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# barcode-reader-composer
+MerryGoblin BarcodeReader
 Barcode reader in PHP. Requires ZBar &amp; Imagick
+========================
+
+[Github](https://github.com/merry-goblin/barcode-reader-composer)
+
+### Sample
+
+[Sample & documentation & unit tests](https://github.com/merry-goblin/barcode-reader)
+
+### Install
+
+- composer require merry-goblin/barcode-reader-composer:"dev-master"
+
+--------------------------
+
+author : [alexandre keller](https://github.com/merry-goblin)
